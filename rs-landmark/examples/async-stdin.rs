c64160862b8709fa0e6e380b74766bc17534be5e
killzoner <twinny902000@yahoo.fr>
2020-01-09 20:24:01 +0100

    wip

--- rs-landmark/examples/async-stdin.rs
+++ rs-landmark/examples/async-stdin.rs
@@ -1,36 +1,56 @@
-use std::io::{self, BufRead};
-//use std::thread;
-
-use futures::{Future, Sink, Stream};
-use futures::stream::BoxStream;
-//use futures::channel::mpsc::channel;
-use async_std::{fs::File, /*io,*/ prelude::*, task};
-use async_std::sync::channel as asyncChannel;
-
-use futures::{
-    SinkExt // for rx/tx send & receive
-};
+use async_std::{io, prelude::*, task};
+use async_std::io::BufReader;
+use async_std::sync::channel;
+use futures::{FutureExt, select, Stream};
+use futures::stream::StreamExt; // for `next`
 
 async fn stdin() -> impl Stream<Item = String> {
-    let (mut tx, rx) = asyncChannel(1);
-    let reader_task = task::spawn(async {
-        let input = io::stdin();
-        for line in input.lock().lines() {
-            match line {
-                Ok(s) => tx.send(s).await,
-                Err(_) => break
+    let (tx, rx) = channel(1);
+    let mut lines = BufReader::new(io::stdin()).lines().fuse();
+    loop {
+        select! {
+            line = lines.next().fuse() => match line {
+                Some(line) => {
+                    match line {
+                        Ok(s) => tx.send(s).await,
+                        Err(_) => break
+                    }
+                }
+                None => break,
             }
         }
-    });
+    }
     rx
 }
 
 fn main() {
-    stdin()
+    /*let future = stdin()
+    .for_each(|string| {
+        println!("{}", string);
+        Ok(())
+    })
+    task::block_on(
+        async {
+            future
+        }
+    )*/
+    task::block_on( 
+        async {
+            let stdin = stdin().await;
+            while let Some(stream) = stdin.next().await {
+                let t:String = stream.clone();
+                println!("{:?}", t);
+            }
+        }
+    );
+    /*async {
+        let lock: () = input.lock().await;
+    };*/
+    /*stdin()
         .for_each(|string| {
             println!("{}", string);
             Ok(())
         })
         .wait()
-        .unwrap();
+        .unwrap();*/
 }
