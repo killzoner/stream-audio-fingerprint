d87aa55ba5ada0c6e3b881e172a5497ca3e588cd
killzoner <twinny902000@yahoo.fr>
2020-01-12 14:59:24 +0100

    wip

--- rs_landmark/examples/async-stdin.rs
+++ rs_landmark/examples/async-stdin.rs
@@ -2,20 +2,29 @@ use async_std::{io, prelude::*, task};
 use async_std::io::BufReader;
 use futures::channel::mpsc;
 use futures::sink::SinkExt;
+use futures::{select, FutureExt};
 
 type Sender<T> = mpsc::UnboundedSender<T>;
 
 async fn stdin(mut tx: Sender<String>) -> () {
     let mut lines = BufReader::new(io::stdin()).lines();
     while let Some(Ok(s)) = lines.next().await {
-        println!("{:?}", s);
         tx.send(s).await.unwrap()
     }
 }
 
 async fn run() {
-    let (stdin_sender, _stdin_receiver) = mpsc::unbounded::<String>();
+    let (stdin_sender, stdin_receiver) = mpsc::unbounded::<String>();
     let handle = task::spawn(stdin(stdin_sender));
+    let mut stdin_receiver = stdin_receiver.fuse();
+    loop {
+        select! {
+            msg = stdin_receiver.next().fuse() => match msg {
+                Some(msg) => println!("{:?}", msg),
+                None => break,
+            },
+        }
+    }
     handle.await
 }
 
