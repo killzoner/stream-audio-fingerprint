9779674495e04da25fdfd5c83b05608da06656e4
killzoner <twinny902000@yahoo.fr>
2020-01-12 15:33:33 +0100

    wip

--- rs_landmark/src/stdin.rs
+++ rs_landmark/src/stdin.rs
@@ -1,23 +1,31 @@
 use async_std::{io, prelude::*, task};
 use async_std::io::BufReader;
 use futures::channel::mpsc;
-//use futures::{FutureExt, select, Stream};
 use futures::sink::SinkExt;
+use futures::{select, FutureExt};
 
 type Sender<T> = mpsc::UnboundedSender<T>;
-//type Receiver<T> = mpsc::UnboundedReceiver<T>;
-//type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 async fn stdin(mut tx: Sender<String>) -> () {
     let mut lines = BufReader::new(io::stdin()).lines();
     while let Some(Ok(s)) = lines.next().await {
-        println!("{:?}", s);
         tx.send(s).await.unwrap()
     }
 }
 
-pub async fn stdin_stream() -> () {
-    let (stdin_sender, _stdin_receiver) = mpsc::unbounded::<String>();
+pub async fn stdin_stream(debug: bool) {
+    let (stdin_sender, stdin_receiver) = mpsc::unbounded::<String>();
     let handle = task::spawn(stdin(stdin_sender));
+    if debug {
+        let mut stdin_receiver = stdin_receiver.fuse();
+        loop {
+            select! {
+                msg = stdin_receiver.next().fuse() => match msg {
+                    Some(msg) => println!("{:?}", msg),
+                    None => break,
+                },
+            }
+        }
+    }
     handle.await
 }
