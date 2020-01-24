ac8f9f8e37e9962f405a5ff04458fb6abf18ed5f
killzoner <twinny902000@yahoo.fr>
2020-01-24 22:38:24 +0100

    wip reading ffmpeg output

---
+++ rs_landmark/examples/stream_ffmpeg_decoder.rs
@@ -0,0 +1,58 @@
+use std::error::Error;
+use std::process::{Command, Stdio};
+use async_std::sync::channel;
+use async_std::{task};
+use futures::{select, FutureExt, StreamExt, AsyncBufReadExt, AsyncBufRead};
+
+const DEBUG: bool = true;
+
+//cat mp3_sample/sample.mp3  | cargo run --release --stream_ffmpeg_decoder
+fn main() {
+
+    // bounded channel for now
+    // let (sender, receiver) = channel::<String>(1024);
+
+    // spawn the command
+    let cmd = "ffmpeg";
+    let decoder = match Command::new(cmd)
+        .args(&[
+            "-i",
+            "pipe:0", 
+            "-acodec",
+            "pcm_s16le",
+            "-ar",
+            "22050",
+            "-ac",
+            "1",
+            "-f",
+            "wav",
+            "-v",
+            "fatal",
+            "pipe:1"
+        ])
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::piped()) 
+        .spawn()
+    {
+        Err(why) => panic!("couldn't spawn ffmpeg: {}", why.description()),
+        Ok(process) => process,
+    };
+        
+    println!("Running {} with id {:?}", cmd, decoder.id());
+
+    let reader = task::spawn(async move {
+        use async_std::io::BufReader;
+        let mut lines = BufReader::new(decoder.stdout.unwrap()).bytes();
+        while let Some(Ok(s)) = lines.next().await {
+            let output = String::from_utf8_lossy(&s);
+            print!("decoder responded with:\n{:?}", output);
+        }
+    });
+
+    task::block_on(async {
+        use async_std::prelude::*;
+        //wait for stdin and reader to have finished
+        reader.await;
+    })
+
+}
